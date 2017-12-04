68609e0d34145767796228db73cff102e90a142b
Arun <arunchauhan54@gmail.com>
2017-12-03 18:28:20 -0600

    Added routing with nodejs

--- app.js
+++ app.js
@@ -1,23 +1,34 @@
 var fs = require('fs');
+var url = require('url');
+
+function renderHTML(path, response) {
+  fs.readFile(path, null, function(error, data) {
+    if (error) {
+      response.writeHead(404);
+      response.write('File not found');
+    } else {
+      response.write(data)
+    }
+    response.end();
+  });
+}
 
 module.exports = {
-
   handleRequest: function(request, response) {
     response.writeHead(200, {
       'Content-Type': 'text/html'
     });
+    var path = url.parse(request.url).pathname;
+    console.log('path');
+    console.log(path);
+    switch (path) {
+      case ('/'):
+        renderHTML('./index.html', response);
+        break
+      case ('/home2'):
+          renderHTML('./index2.html', response);
 
-    fs.readFile('./index.html', null, function(error, data) {
-      if (error) {
-        response.writeHead(404);
-        response.write('File not found')
-      } else {
-        response.write(data);
-      }
-      response.end();
-
-    })
-
+    }
   }
 
 }
